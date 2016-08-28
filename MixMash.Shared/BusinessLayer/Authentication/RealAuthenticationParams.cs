2f3fc4db4fa4e6eee5acf9781b7f8d9951ae70c6
mjknowles <mjknowles23@gmail.com>
2016-08-27 19:16:34 -0500

    Added correct configurations to be able to talk to the Spotify API endpoint

---
+++ MixMash.Shared/BusinessLayer/Authentication/RealAuthenticationParams.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixMash.Shared.BL.Authentication
+{
+    public class RealAuthenticationParams : AuthenticationParams
+    {
+        public RealAuthenticationParams()
+        {
+            ClientId = "your_client_id_here";
+            Scope = "";
+            AuthorizeUrl = new Uri("https://accounts.spotify.com/authorize");
+            AccessTokenUrl = new Uri("https://accounts.spotify.com/api/token");
+            RedirectUrl = new Uri("com.mjknowles.mixmash://callback/");
+        }
+    }
+}
